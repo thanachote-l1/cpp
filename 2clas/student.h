a96e808ee93f3774042fecdf970bd92ef85cc6ca
Thanachote Li <thanachoteli.2006@gmail.com>
2024-10-03 17:23:01 +0700

    Add note

--- 2clas/student.h
+++ 2clas/student.h
@@ -4,7 +4,12 @@
 
 class Student{
 private:
-  c
+  static constexpr def_name = "Unknown";
+  static constexpr def_age = "0";
+  static constexpr def_birthday = "00/00/0000";
+
+public:
+  
 };
 
 
---
+++ note/cpp_note.md
@@ -0,0 +1,6 @@
+
+# Large
+
+## Medium
+
+
